1b8a4b8ff79069dbf2481493b967321dc6162907
Andreas Behnke <ab@andreasbehnke.com>
2015-06-25 17:37:55 +0200

    REST endpoint list of document names

--- src/main/java/org/pdfserv/Application.java
+++ src/main/java/org/pdfserv/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 	
-	public static String DOCUMENT_FOLDER = System.getProperty("user.dir") + File.separator + "documents";
+	public static File DOCUMENT_FOLDER = new File(System.getProperty("user.dir") + File.separator + "documents");
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/org/pdfserv/DocumentResource.java
+++ src/main/java/org/pdfserv/DocumentResource.java
@@ -1,17 +1,28 @@
 package org.pdfserv;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.springframework.stereotype.Component;
 
 @Component
 @Path("/document")
+@Produces(MediaType.APPLICATION_JSON)
 public class DocumentResource {
 
 	@GET
-    public String helloWorld() {
-    	return "Hello World!";
+	public List<String> documentList() {
+    	return Arrays.asList(
+    			Application.DOCUMENT_FOLDER.list( 
+    					(File dir, String name) -> { return name.endsWith("pdf") || name.endsWith("PDF"); }
+    			)
+    		);
     }
 
 }
