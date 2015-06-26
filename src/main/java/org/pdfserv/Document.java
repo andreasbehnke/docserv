a71f031a7f827b1b549bba83711ede3bcace2b0e
Andreas Behnke <ab@andreasbehnke.com>
2015-06-26 09:58:42 +0200

    REST endpoint for retrieving document meta data

---
+++ src/main/java/org/pdfserv/Document.java
@@ -0,0 +1,35 @@
+package org.pdfserv;
+
+
+public class Document {
+
+	private String name;
+	
+	private int pageCount;
+	
+	private String lastModified;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPageCount() {
+		return pageCount;
+	}
+
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+
+	public String getLastModified() {
+		return lastModified;
+	}
+
+	public void setLastModified(String lastModified) {
+		this.lastModified = lastModified;
+	}
+}
--- src/main/java/org/pdfserv/DocumentResource.java
+++ src/main/java/org/pdfserv/DocumentResource.java
@@ -1,20 +1,28 @@
 package org.pdfserv;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.springframework.stereotype.Component;
 
 @Component
 @Path("/document")
 @Produces(MediaType.APPLICATION_JSON)
 public class DocumentResource {
+	
+	private final static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
 
 	@GET
 	public List<String> documentList() {
@@ -25,4 +33,31 @@ public class DocumentResource {
     		);
     }
 
+	private File openFile(String documentName) {
+		return new File(Application.DOCUMENT_FOLDER, documentName);
+	}
+	
+	private int readPageCount(File documentFile) throws IOException {
+		try (PDDocument pdfDocument = PDDocument.load(documentFile)) {
+			return pdfDocument.getNumberOfPages();
+		}
+	}
+	
+	private Date getLastModified(File documentFile) {
+		return new Date(documentFile.lastModified());
+	}
+	
+	@GET
+	@Path("/{documentName}")
+	public Document getDocument(@PathParam("documentName") String documentName) throws IOException {
+		File documentFile = openFile(documentName);
+		if (!documentFile.exists()) {
+			throw new NotFoundException();
+		}
+		Document document = new Document();
+		document.setName(documentName);
+		document.setLastModified(DATE_FORMAT.format(getLastModified(documentFile)));
+		document.setPageCount(readPageCount(documentFile));
+		return document;
+	}
 }
