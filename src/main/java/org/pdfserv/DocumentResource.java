435ceb6c8e55fe3b6be030f11fa4d3bbb08dc6ee
Andreas Behnke <ab@andreasbehnke.com>
2015-06-26 17:51:12 +0200

    added pdf page rendering

--- src/main/java/org/pdfserv/DocumentResource.java
+++ src/main/java/org/pdfserv/DocumentResource.java
@@ -1,24 +1,31 @@
 package org.pdfserv;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import javax.imageio.ImageIO;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.EntityTag;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Request;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -37,8 +44,12 @@ public class DocumentResource {
     		);
     }
 
-	private File openFile(String documentName) {
-		return new File(Application.DOCUMENT_FOLDER, documentName);
+	private File openDocumentFile(String documentName) {
+		File documentFile = new File(Application.DOCUMENT_FOLDER, documentName);
+		if (!documentFile.exists()) {
+			throw new NotFoundException();
+		}
+		return documentFile;
 	}
 	
 	private int readPageCount(File documentFile) throws IOException {
@@ -70,15 +81,43 @@ public class DocumentResource {
 	@GET
 	@Path("/{documentName}")
 	public Response getDocument(@PathParam("documentName") String documentName, @Context Request request) throws IOException {
-		File documentFile = openFile(documentName);
-		if (!documentFile.exists()) {
-			throw new NotFoundException();
-		}
+		File documentFile = openDocumentFile(documentName);
 		EntityTag eTag = createETag(documentFile);
 		if (isUnchanged(request, eTag)) {
 			return Response.notModified(eTag).build();
 		} else {
 			return Response.ok(createDocument(documentName, documentFile)).tag(eTag).build();
+		}
+	}
+	
+	private StreamingOutput createPage(File documentFile, int pageNumber, int pageResolution) {
+		return (OutputStream output) -> {
+			try (PDDocument pdfDocument = PDDocument.load(documentFile)) {
+				int pageNumberInternal = Math.min(pageNumber, pdfDocument.getNumberOfPages() - 1);
+				PDFRenderer renderer = new PDFRenderer(pdfDocument);
+				BufferedImage pageImage = renderer.renderImageWithDPI(pageNumberInternal, pageResolution, ImageType.RGB);
+				ImageIO.write(pageImage, "PNG", output);
+			} finally {
+				output.close();
+			}
+		};
+	}
+	
+	@GET
+	@Path("/{documentName}/{pageNumber}")
+	@Produces("image/png")
+	public Response getPage(
+			@PathParam("documentName") String documentName, 
+			@PathParam("pageNumber") int pageNumber,
+			@QueryParam("pageResolution") Integer pageResolution,
+			@Context Request request) throws IOException {
+		File documentFile = openDocumentFile(documentName);
+		EntityTag eTag = createETag(documentFile);
+		if (isUnchanged(request, eTag)) {
+			return Response.notModified(eTag).build();
+		} else {
+			int pageResolutionInternal = (pageResolution == null) ? 72 : pageResolution;
+			return Response.ok(createPage(documentFile, pageNumber, pageResolutionInternal)).tag(eTag).build();
 		}
 	}
 }
