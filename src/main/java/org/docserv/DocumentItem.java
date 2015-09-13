19816de1b2e1469a0180bb3a9653d3a9f417d99e
andreasbehnke <andreasbehnke@XPS-13-9343>
2015-09-13 12:42:59 +0200

    Add document name to document item and change javascript to use new directory resource

--- src/main/java/org/docserv/DocumentItem.java
+++ src/main/java/org/docserv/DocumentItem.java
@@ -2,16 +2,26 @@ package org.docserv;
 
 public class DocumentItem {
 	
+	private String name;
+	
 	private String path;
 	
 	private boolean isFolder;
 	
 	public DocumentItem() {}
 
-	public DocumentItem(String path, boolean isFolder) {
-		super();
+	public DocumentItem(String name, String path, boolean isFolder) {
+		this.name = name;
 		this.path = path;
 		this.isFolder = isFolder;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getPath() {
